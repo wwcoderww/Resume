e738a67bd76dda930a4414d680d5be1ae3c39130
wwcoderww <wwcoderww@gmail.com>
2025-05-14 17:16:43 -0500

    Added more links

--- src/features/morebookmarks/MoreBookmarks.jsx
+++ src/features/morebookmarks/MoreBookmarks.jsx
@@ -17,12 +17,18 @@ export default function MoreBookmarks() {
   return (
     <MainContainer>
       <StyledUl>
+        <Bookmark link={"https://www.theodinproject.com/dashboard"}>
+          The Odin Project
+        </Bookmark>
         <Bookmark link={"https://react.dev/"}>React Docs</Bookmark>
         <Bookmark link={"https://tailwindcss.com/"}>Tailwind</Bookmark>
       </StyledUl>
       <StyledUl>
-        <Bookmark link={"https://www.theodinproject.com/dashboard"}>
-          The Odin Project
+        <Bookmark link={"https://verified.capitalone.com/auth/signin"}>
+          Capital One
+        </Bookmark>
+        <Bookmark link={"https://voice.google.com/u/0/messages"}>
+          Google Voice
         </Bookmark>
       </StyledUl>
     </MainContainer>
